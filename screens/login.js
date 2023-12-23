98c296626b039e1f5fc3f48fe4839d7855cac2ac
ggwayne <ggwynneth97@gmail.com>
2023-12-23 15:56:16 +0800

    Initial commit

--- screens/login.js
+++ screens/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 
@@ -8,71 +8,95 @@ export default function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [buttonColor, setButtonColor] = useState('green');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const navigation = useNavigation();
 
-  const handleLogin = (credentials) => {
-    const url = 'https://agile-forest-15442-13dfc75c01e7.herokuapp.com/user/login'
+  const handleLogin = () => {
+    if (!username || !password) {
+      setErrorMessage('Please fill all empty fields');
+      return;
+    }
+
+    setErrorMessage(null);
+
+    setIsSubmitting(true);
+
+    const url = 'https://agile-forest-15442-13dfc75c01e7.herokuapp.com/user/login';
 
     axios
-    .post(url, credentials)
-    .then((response) => {
-      const result = response.data;
-      const {message, status, data} = result;
-    })
-    .catch(error => {
-      console.log(error.JSON());
-    })
-  }
+      .post(url, { username, password })
+      .then((response) => {
+        const result = response.data;
+        const { message, status, data } = result;
+
+        if (status !== 'SUCCESS') {
+          setErrorMessage(message);
+        } else {
+          navigation.navigate('Welcome', { ...data[0] });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage('Error. Check your network and try again');
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
 
   return (
     <View style={styles.appContainer}>
-         <StatusBar style="auto" />
+      <StatusBar style="auto" />
       <View style={styles.inputContainer}>
-     
-
         <Text style={[styles.title, styles.heading]}>LOGIN</Text>
         <Text style={styles.text}>Welcome to HydroApp</Text>
         <TextInput
           style={[styles.textInput]}
-          placeholder='Username'
+          placeholder="Username"
           placeholderTextColor={'gray'}
           onChangeText={(text) => setUsername(text)}
         />
         <TextInput
           style={[styles.textInput]}
-          placeholder='Password'
+          placeholder="Password"
           placeholderTextColor={'gray'}
           secureTextEntry={true}
           onChangeText={(text) => setPassword(text)}
         />
-        
-        <TouchableOpacity onPress={() =>
-    navigation.navigate('ScreenLevels')}>
-          <View style={[styles.button, { backgroundColor: buttonColor }]}>
-            <Text style={[styles.buttonText]}>LOGIN</Text>
-          </View>
-        </TouchableOpacity>
+
+        {isSubmitting ? (
+          <ActivityIndicator size="large" color="#999999" />
+        ) : (
+          <TouchableOpacity onPress={handleLogin}>
+            <View style={[styles.button, { backgroundColor: buttonColor }]}>
+              <Text style={[styles.buttonText]}>LOGIN</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <View style={styles.errorContainer}>
+        {errorMessage && <Text style={[styles.errorText, { color: 'red' }]}>{errorMessage}</Text>}
       </View>
 
       <View style={styles.signUpContainer}>
         <Text style={styles.smallText}>No account? </Text>
-        <TouchableOpacity onPress={() =>
-    navigation.navigate('Register')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
           <Text style={[styles.smallText, styles.linkGreen]}>Register Here</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.signUpContainer}>
-        <TouchableOpacity onPress={() =>
-    navigation.navigate('ForgotPassword')}>
+        <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
           <Text style={[styles.smallText, styles.linkGreen]}>Forgot Password?</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -143,5 +167,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
   },
-  
 });
